1dd54ea944214b4832245cc7ec8e4b7192fc2b4f
bruno <bruno@host.docker.internal>
2024-04-23 21:45:02 -0300

    primeiro commit

--- src/main/ContaTerminal.java
+++ src/main/ContaTerminal.java
@@ -1,7 +1,29 @@
 package main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 	public static void main(String[] args) {
-		System.out.println("teste");
+		
+		 Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		 
+		 System.out.println("Seja Bem Vindo !");
+		 System.out.println("Digite o número da Agência: ");
+		 int numero = scanner.nextInt();
+		 
+		 System.out.println("Digite a Agência: ");
+		 String  agencia = scanner.next();
+		 
+		 System.out.println("Digite o Nome: ");
+		 String  nome = scanner.next();
+		 
+		 scanner.nextLine();
+		 
+		 System.out.println("Digite o Saldo: ");
+		 double saldo = scanner.nextDouble();
+		 
+		 System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + numero + ", conta " +  agencia + " e seu saldo " + saldo + " já está disponível para saque.");	 
+		 
 	}
 }
